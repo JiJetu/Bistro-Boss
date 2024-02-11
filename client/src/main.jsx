c50551a5af89111f12b310239aefe641956b7ec3
JiJetu <jijetu2@gmail.com>
2024-02-11 19:33:38 +0600

    added navbar and footer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './Routh/Routh.jsx'
+import { router } from './Route/Route.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
