94ef4f0237292ce2c46d18a75b03c8cb76b9775b
JiJetu <jijetu2@gmail.com>
2024-02-13 16:04:06 +0600

    use get operation for menu and reviews

--- server/index.js
+++ server/index.js
@@ -28,7 +28,35 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        
+        const BossDB = client.db("BossDB");
+        const menu = BossDB.collection("menu");
+        const review = BossDB.collection("review");
+
+        // menu
+        // find all menu
+        app.get('/menu', async (req, res) => {
+            try {
+                const result = await menu.find().toArray()
+                res.send(result)
+            } 
+            catch (error) {
+                console.log(error);
+                res.send(error)
+            }
+        })
+
+        // review
+        // find all review
+        app.get('/review', async(req, res) => {
+            try {
+                const result = await review.find().toArray()
+                res.send(result)    
+            } 
+            catch (error) {
+                console.log(error);
+                res.send(error)
+            }
+        }) 
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
