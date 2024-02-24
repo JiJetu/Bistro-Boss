da21dd238e5a7fd91d88593a9292202432ec9057
JiJetu <jijetu2@gmail.com>
2024-02-24 14:43:53 +0600

    created delete method for menu api to delete item from the menu api

--- server/index.js
+++ server/index.js
@@ -174,6 +174,15 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result)
+        })
+
+
+
         // review
         // find all review
         app.get('/review', async (req, res) => {
